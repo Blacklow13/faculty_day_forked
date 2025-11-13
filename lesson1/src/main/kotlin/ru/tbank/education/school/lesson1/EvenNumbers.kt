beb3f862bdf60c2cac04f38228c67c0545876246
Blacklow13 <aysel.baratova@mail.ru>
2025-11-13 15:55:03 +0300

    Revert "материалы 1 и 2 занятия"
    This reverts commit 073bc22c46c8427f2c41ee3a07849cb221fedfb9.
    
    # Conflicts:
    #	lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/EvenNumbers.kt
    #	lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/Main.kt

--- lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/EvenNumbers.kt
+++ lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/EvenNumbers.kt
@@ -3,25 +3,10 @@ package ru.tbank.education.school.lesson1
 /**
  * Сумма четных чисел.
  */
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
+fun sumEvenNumbers(numbers: Array<Int>) =
+    numbers.filter { it % 2 == 0 }.sum()
 
-fun sumEvenNumbers(numbers: Array<Int>) = numbers.filter{it % 2 == 0}.sum()
-
-fun main(){
-    val array = arrayOf(1, 2, 3, 4, 6)
-    println(sumEvenNumbers(array))
-=======
-fun sumEvenNumbers(numbers: Array<Int>): Int {
-    TODO()
->>>>>>> parent of 073bc22 (материалы 1 и 2 занятия)
-=======
-fun sumEvenNumbers(numbers: Array<Int>): Int {
-    TODO()
->>>>>>> parent of 073bc22 (материалы 1 и 2 занятия)
-=======
-fun sumEvenNumbers(numbers: Array<Int>): Int {
-    TODO()
->>>>>>> parent of 073bc22 (материалы 1 и 2 занятия)
+fun main() {
+    val numbers = arrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+    println(sumEvenNumbers(numbers))
 }
